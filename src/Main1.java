c3820e98e3d4149109b22009394d4d7db5c37a5d
OkVanya <mister.okladnikov@yandex.ru>
2020-02-22 18:25:32 +0300

    1 коммит

--- src/Main1.java
+++ src/Main1.java
@@ -1,5 +1,7 @@
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("Lesson1");
+        System.out.println(1324);
+        System.out.println();
     }
 }
