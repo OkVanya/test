b91d80149958152598573dbedc1040c192bd87e2
OkVanya <mister.okladnikov@yandex.ru>
2020-04-06 23:04:30 +0300

    HomeWork1

--- src/Main1.java
+++ src/Main1.java
@@ -1,26 +1,17 @@
 public class Main1 {
     public static void main(String[] args) {
-        byte b = 1;
-        short t = 5;
-        int i = 4;
-        long l = 6;
-        float f = (float) 2.35;
-        double d = 3.25;
-        boolean bool = false;
-        char ch = 'a';
 
-        task3(2,1,12,6);
-        task4(8,7);
-        task5(-1);
-        task6(2);
-        task7("Ирина");
+        task1(2.5,1,12,6);
+        task2(8,7);
+        task3(-1);
+        task4("Ирина");
     }
 
-    public static int task3 (int a, int b, int c, int d) {
+    public static double task1 (double a, int b, int c, int d) {
         return a *(b +(c /d));
     }
 
-    public static boolean task4 (int g, int j) {
+    public static boolean task2 (int g, int j) {
         int sum = (g + j);
         boolean bool = true;
         if (sum >= 10 && sum <= 20 ) {
@@ -30,21 +21,15 @@ public class Main1 {
         }
     }
 
-    public static void task5 (int x) {
+    public static void task3 (int x) {
         if(x <= 0) {
             System.out.println("Это положительное число");
         } else {
             System.out.println("Это отрицательное число");
         }
     }
-    public static boolean task6 (int x) {
-        if(x < 0) {
-            return true;
-        }
-        return false;
-    }
 
-    public static void task7 (String y) {
+    public static void task4 (String y) {
         System.out.println("Привет," + y);
     }
 
